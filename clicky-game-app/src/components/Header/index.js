4067a6857573f1318d21a3465077e782057b27ec
erdakjo <ekenney60@gmail.com>
2019-03-08 22:15:53 -0700

    adding muppet game files

---
+++ clicky-game-app/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1> Muppet Clicky Game!</h1>
+      <h2>Click on an  Muppet-image to earn points, but don't click on any Muppet more than once!</h2>
+    </header>
+  );
+}
+
+export default Header;
