ad9975f502adad3e2128b1734501925b6251f53e
erdakjo <ekenney60@gmail.com>
2019-03-08 23:43:14 -0700

    Updates

---
+++ precache-manifest.91c3f8309e6db78e47992564b82da128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.895984e4.js"
+  },
+  {
+    "revision": "50973adff7b35448d343",
+    "url": "/clicky-game/static/js/main.fa2cc58e.chunk.js"
+  },
+  {
+    "revision": "02bedce189d00f81d660",
+    "url": "/clicky-game/static/js/2.13e550dc.chunk.js"
+  },
+  {
+    "revision": "50973adff7b35448d343",
+    "url": "/clicky-game/static/css/main.bd4651e6.chunk.css"
+  },
+  {
+    "revision": "0d890b5542666943e4912c0826296fea",
+    "url": "/clicky-game/index.html"
+  }
+];
